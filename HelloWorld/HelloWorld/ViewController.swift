02c8bbe25ada640cba542c930553ecf5d8f15bbc
wyn <k022298@gmail.com>
2020-03-17 23:51:26 +0800

    Add 4 buttons and alert functions with each other

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by wyn on 2020/3/17.
+//  Copyright © 2020 Wyn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+        override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showMessage(sender: UIButton){
+        let emojiDict: [String: String] = ["👾":"alien",
+                                           "👻":"ghost",
+                                           "🤓":"dumb",
+                                           "🤖":"robot"]
+        let selectionButton = sender
+        if let wordToLookup = selectionButton.titleLabel?.text{
+            let meaning = emojiDict[wordToLookup]
+            
+            let alertController = UIAlertController(title: "Meaning", message: meaning, preferredStyle: UIAlertController.Style.alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
+//        let alertController = UIAlertController(title: "Welcome to my First APP", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+//        alertController.addAction(UIAlertAction(title:"OK", style: UIAlertAction.Style.default,handler: nil))
+//        present(alertController, animated: true, completion: nil)
+    }
+
+}
+
